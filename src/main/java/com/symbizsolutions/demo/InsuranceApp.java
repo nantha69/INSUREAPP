6769930987ba85a8179d627630ea12ded4561358
kannanganesh <ganesh.kannan@symbizsolutions.com>
2021-04-30 22:31:36 +0800

    my first commit

--- src/main/java/com/symbizsolutions/demo/InsuranceApp.java
+++ src/main/java/com/symbizsolutions/demo/InsuranceApp.java
@@ -1,11 +1,14 @@
 package com.symbizsolutions.demo;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Log4j2
 @SpringBootApplication
 public class InsuranceApp {
     public static void main(String... args) {
         SpringApplication.run(InsuranceApp.class, args);
+        log.info("app working");
+
     }
 }
