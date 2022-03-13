4e324dfdbd9e9335513955bd4f249dbaa5268877
kannanganesh <ganesh.kannan@symbizsolutions.com>
2022-03-13 12:17:56 +0800

    msg changed for products advise

--- src/main/resources/static/product.js
+++ src/main/resources/static/product.js
@@ -13,7 +13,7 @@ const handleLoad = async (e) => {
     const id = form.elements['id'].value
     if (id) {
         const res = await loadEntity(`/product/${id}`);
-        form.elements["message"].value = "Product loaded from database";
+        form.elements["message"].value = "Insurance products advised";
     }
     else {
       form.elements["message"].value = "Please key in the id to load";
