c7a27e2eb78a367723d8771f37aecb8c950e2e49
kannanganesh <ganesh.kannan@symbizsolutions.com>
2022-03-19 11:14:38 +0800

    msg changed to policy advisor bot

--- src/main/resources/static/customer.js
+++ src/main/resources/static/customer.js
@@ -31,7 +31,7 @@ const handleCountryChange = async (e) => {
             const json = await response.json();
             console.log(json);
             showTable("productsTable",json);
-            form.elements["message"].value = "Recommended Insurance Policies";
+            form.elements["message"].value = "Results from Policy Advisor Bot";
         }
         else {
             form.elements["message"].value = `Server returned error ${response.status}`;
